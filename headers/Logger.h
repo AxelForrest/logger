adf62faabeff196e84e7cd9df0fc79592f2e8ffa
AlexForrest <nobodycanacrossit@yandex.ru>
2015-11-11 02:05:05 +0300

    nearly for pre alpha release

--- headers/Logger.h
+++ headers/Logger.h
@@ -8,6 +8,7 @@
 #define WAIT_FOR_DATA 2
 #define WAIT_FOR_COMMAND 3
 #define WAIT_FOR_HELLO 4
+#define RECIVE_SUCCESFUL 5
 #define LENGTH_OF_COMMAND 9
 #define LIVE_TIME 1
 #define COUNT_OF_TRY 10
@@ -41,9 +42,9 @@ namespace logger
 			return ans;
 		}
 
-		void MakeHeader(char* strdest, char ch, int size /* without length of command*/, int connection_id) 
+		void MakeHeader(char* strdest, char ch, int size /* without length of command*/, int connection_id)
 		{
-			strdest[0] = "D";
+			strdest[0] = ch;
 			IntToFourChar(&strdest[1], size);
 			IntToFourChar(&strdest[5], connection_id); // for nat;
 		}
@@ -57,11 +58,14 @@ namespace logger
 				IntToFourChar(&temp[LENGTH_OF_COMMAND], num);
 				memcpy(&temp[LENGTH_OF_COMMAND + 4], &allData[num*(SIZE_OF_MSG_MAX)], min((SIZE_OF_MSG_MAX), allData.length() - (num * (SIZE_OF_MSG_MAX))));
 				tsize = min(SIZE_OF_MSG_MAX, allData.length() - (num)* (SIZE_OF_MSG_MAX)) + 4 + LENGTH_OF_COMMAND;
+				printf("Sending data...\n");
+			//	for (int i = 0; i < tsize; ++i)
+			//		printf("%c\n", temp[i]);
 				pSock->Sendto(temp, tsize, dest);
 			}
 			else
 			{
-				MakeHeader(temp, 'S', 0, id);
+				MakeHeader(temp, 'S', numberof, id);
 				tsize = LENGTH_OF_COMMAND;
 				pSock->Sendto(temp, LENGTH_OF_COMMAND, dest);
 			}
@@ -91,13 +95,21 @@ namespace logger
 		}
 
 		void ReciveHello(int newID)
-		{ 
+		{
 			if (id < 0)
 			{
 				id = newID;
 			}
-			printf("Connection with %d:%d set. New id = %d\n", dest.GetIP(), dest.GetPort(), newID);
+			printf("Connection with %s set. New id = %d\n", data(IP()), id);
 			status = WAIT_FOR_COMMAND;
+			if (reciveSuc == WAIT_FOR_HELLO)
+			{
+				reciveSuc = 0;
+				time(&lastSucces);
+				tryies = 0;
+				numberof = 0;
+				SendPart(numberof);
+			}
 		}
 
 		void SendAproove()
@@ -125,14 +137,30 @@ namespace logger
 
 	public:
 
-		void GetFromHeader(const char* strsource, char& command, int& size, int& id) 
+		string IP() 
 		{
-			command = strsource[0];
-			size = IntFromStr(strsource[1]);
-			id = IntFromStr(strsource[5]);
+			char ip[4];
+			char res[30];
+			IntToFourChar(ip, dest.GetIP());
+			sprintf_s(res, "%u.%u.%u.%u:%u", ip[3], ip[2], ip[1], ip[0], dest.GetPort());
+			return res;
 		}
 
-		void SendHello(int forConnectID = id) 
+		void GetFromHeader(const char* strsource, char& command, int& size, int& id)
+		{
+			command = strsource[0];
+			size = IntFromStr(&strsource[1]);
+			id = IntFromStr(&strsource[5]);
+		}
+
+		void SendHello() 
+		{
+			MakeHeader(temp, 'H', id, id);
+			tsize = LENGTH_OF_COMMAND;
+			pSock->Sendto(temp, tsize, dest);
+		}
+
+		void SendHello(int forConnectID)
 		{
 			MakeHeader(temp, 'H', id, forConnectID);
 			tsize = LENGTH_OF_COMMAND;
@@ -161,8 +189,8 @@ namespace logger
 		void WorkOn(char* data)
 		{
 			char action;
-			int size, id;
-			GetFromHeader(data, action, size, id);
+			int size, idinfo;
+			GetFromHeader(data, action, size, idinfo);
 			if (action == 'H')
 			{
 				pSock->Recive(data, LENGTH_OF_COMMAND, dest);
@@ -173,11 +201,15 @@ namespace logger
 			}
 			else
 			{
+				size += LENGTH_OF_COMMAND;
 				pSock->Recive(data, size, dest);
+				printf("Reciving data\n");
+			//	for (int i = 0; i < size; ++i)
+			//		printf("%c\n", data[i]);
 				if (status == WAIT_FOR_APROOV && action == 'A')
 				{
-					printf("%c%c%c%c%c\n", action, data[1], data[2], data[3], data[4]);
-					printf("%d\n", IntFromStr(&data[1]));
+					//printf("%c%c%c%c%c\n", action, data[1], data[2], data[3], data[4]);
+					//printf("%d\n", IntFromStr(&data[1]));
 					if ((numberof) == IntFromStr(&data[1]))
 					{
 						ReciveAproove();
@@ -187,7 +219,7 @@ namespace logger
 				}
 				else if (status == WAIT_FOR_DATA)
 				{
-					printf("Work with data %c\n", action);
+					//printf("Work with data %c\n", action);
 					if (action == 'D')
 					{
 						data[size] = 0;
@@ -204,17 +236,17 @@ namespace logger
 
 						}
 					}
-					else if (data[0] == 'S')
+					else if (action == 'S')
 					{
 						time(&lastSucces);
 						SendAproove();
-						reciveSuc = 1;
+						reciveSuc = RECIVE_SUCCESFUL;
 						tryies = 0;
 					}
 				}
 				else if (status == WAIT_FOR_COMMAND)
 				{
-					if (data[0] == 'D')
+					if (action == 'D')
 					{
 						status = WAIT_FOR_DATA;
 						data[size] = 0;
@@ -226,12 +258,12 @@ namespace logger
 							tryies = 0;
 						}
 					}
-					else if (data[0] == 'S')
+					else if (action == 'S')
 					{
 						numberof == 0;
 						allData.clear();
 						SendAproove();
-						reciveSuc = 1;
+						reciveSuc = RECIVE_SUCCESFUL;
 						time(&lastSucces);
 						tryies = 0;
 					}
@@ -241,7 +273,7 @@ namespace logger
 
 		bool IsReciveSuccesfull()
 		{
-			return reciveSuc;
+			return reciveSuc == RECIVE_SUCCESFUL;
 		}
 
 		void RelaxRecive()
@@ -260,9 +292,20 @@ namespace logger
 		{
 			if (status == WAIT_FOR_COMMAND)
 			{
+				time(&lastSucces);
+				tryies = 0;
 				numberof = 0;
 				allData = data;
 				SendPart(numberof);
+				return true;
+			}
+			else if (status == WAIT_FOR_HELLO && reciveSuc != WAIT_FOR_HELLO)
+			{
+				time(&lastSucces);
+				tryies = 0;
+				numberof = 0;
+				reciveSuc = WAIT_FOR_HELLO;
+				allData = data;
 				return true;
 			}
 			else
@@ -284,6 +327,10 @@ namespace logger
 				else
 					return true;
 			}
+			else if (status == WAIT_FOR_COMMAND && difftime(now, lastSucces) > LIVE_TIME * 100) 
+			{
+				return true;
+			}
 			return false;
 		}
 
@@ -295,9 +342,8 @@ namespace logger
 		Socket_UDP sock;
 		IPAdress ClientAdress;
 		map<int, Connection> connections;
-		map<int, IPAdress> idtoIP;
 		char cData[MAX_LENGTH_OF_MSG];
-		map<IPAdress, bool> forDelete;
+		map<int, bool> forDelete;
 		time_t now, last;
 		//			Connection currentConnection;
 	public:
@@ -331,100 +377,177 @@ namespace logger
 			return true;
 		}
 
-		int SendNewConnection(const IPAdress& dest)
+		int AskNewConnection(const IPAdress& dest)
 		{
 			int connectID = -(rand() + 1);
-			iptoID[connectID] = dest;
 			connections[connectID] = Connection(dest, &sock, connectID);
 			connections[connectID].SendHello();
 			return connectID;
 		}
 
-		int RecivedNewConnection(const IPAdress& dest, int connectID) 
+		int MakeNewConnection(const IPAdress& dest)
+		{
+			int result = AskNewConnection(dest);
+			WaitForConnect(result);
+			return result;
+		}
+
+		int WaitForConnect(int & connectionID)
+		{
+			for (int i = 0; i < 10000000; ++i)
+			{
+				if (sock.Recive(&cData[0], LENGTH_OF_COMMAND, ClientAdress, MSG_PEEK))
+				{
+					char comm; int size, id;
+					Connection().GetFromHeader(&cData[0], comm, size, id);
+					if (comm == 'H')
+					{
+						if (connections.find(id) == connections.end())
+						{
+							id = RecivedNewConnection(ClientAdress, id);
+						}
+						else
+						{
+							connections[size] = connections[id];
+							connections.erase(id);
+							if (connectionID == id)
+							{
+								connectionID = size;
+							}
+							id = size;
+						}
+					}
+					if (connections.find(id) != connections.end())
+					{
+						connections[id].WorkOn(&cData[0]);
+						if (id == connectionID)
+						{
+							return true;
+						}
+					}
+					else { sock.Recive(&cData[0], LENGTH_OF_COMMAND, ClientAdress); }
+				}
+			}
+			return false;
+		}
+
+		int RecivedNewConnection(const IPAdress& dest, int connectID)
 		{
 			int legalID;
 			do {
 				legalID = rand();
 			} while (connections.find(legalID) != connections.end());
-			connections[legalID] = Connection(dest, &sock, connectID);
+			connections[legalID] = Connection(dest, &sock, legalID);
 			connections[legalID].SendHello(connectID);
 			return legalID;
 		}
 
 		bool ISReciveMSG(string &message, int& source)
 		{
-			if (sock.Recive(&cData[0], LENGTH_OF_COMMAND, ClientAdress, MSG_PEEK)) 
+			if (sock.Recive(&cData[0], LENGTH_OF_COMMAND, ClientAdress, MSG_PEEK))
 			{
 				char comm; int size, id;
 				Connection().GetFromHeader(&cData[0], comm, size, id);
 				if (comm == 'H')
 				{
-					if (connections.find(id) == connections.end()) 
+					if (connections.find(id) == connections.end())
 					{
 						id = RecivedNewConnection(ClientAdress, id);
-						idtoIP[id] = ClientAdress;
 					}
-					else 
+					else
 					{
-						connections[size] = connnections[id];
-						idtoIP[size] = ClientAdress;
+						connections[size] = connections[id];
 						connections.erase(id);
-						idtoIP.erase(id);
 						id = size;
+						source = id;
 					}
-				} 
-				connections[id].WorkOn(&cData[0], LENGTH_OF_COMMAND);
-				if (connections.begin() != connections.end() && connections[id].IsReciveSuccesfull())
-				{
-					message = connections[id].Message();
-					source = id;
-					connections[id].RelaxRecive();
-					return true;
 				}
+				if (connections.find(id) != connections.end())
+				{
+					connections[id].WorkOn(&cData[0]);
+					if (connections.begin() != connections.end() && connections[id].IsReciveSuccesfull())
+					{
+						message = connections[id].Message();
+						source = id;
+						connections[id].RelaxRecive();
+						return true;
+					}
+				}
+				else { sock.Recive(&cData[0], LENGTH_OF_COMMAND, ClientAdress); }
 			}
 			CheckConnection();
 			return false;
 		}
 
-		bool SendMessageW(const string &msg, const int & id)
+		bool SendMessageW(const string &msg, const int & id, bool NO_WARN = false)
 		{
 			if (connections.find(id) == connections.end())
 			{
-				printf("Bad id = %d\n", id);
-				return false
-			}
-			else if (id < 0) 
+				printf("Error could start sending message:Bad id = %d\n", id);
+				return false;
+			} if (!NO_WARN && id < 0)
 			{
-				printf("Sorry not yet connected id = %d\n", id);
+				printf("Error not initiated yet:Bad id = %d, message = %s\n", id, data(msg));
 				return false;
 			}
 			connections[id].SendMSG(&msg[0]);
+			return true;
 		}
 
-		bool NextReciveFrom(string &msg, IPAdress & source)
+		bool NextReciveFrom(string &msg, int & id, int timelimitsec = 100)
 		{
-			while (NextReciveMSG(msg, ClientAdress) && !(ClientAdress == source));
-			return (ClientAdress == source);
+			int temp;
+			time(&last);
+			for (; difftime(now, last) < timelimitsec && NextReciveMSG(msg, temp) && temp != id; time(&now));
+			return (temp == id);
 		}
 
 
-		bool UntilSendMessage(const string &msg, IPAdress & dest)
+		bool UntilSendMessage(const string &msg, const int & dest)
 		{
-			SendMessageW(msg, dest);
+			if (connections.find(dest) == connections.end())
+			{
+				printf("Error could start sending message:Bad id = %d\n", dest);
+				return false;
+			}
 			time(&now);
-			while ((connections[dest].Status() == WAIT_FOR_APROOV) && !(connections[dest].NoChances(now)))
+			if (connections[dest].NoChances(now)) 
+			{
+				printf("Session is closed for time limit unactivity: in id = %d\n", dest);
+				connections.erase(dest);
+				return false;
+			}
+			SendMessageW(msg, dest, true);
+			while ((connections[dest].Status() == WAIT_FOR_APROOV || connections[dest].Status() == WAIT_FOR_HELLO) && !(connections[dest].NoChances(now)))
 			{
 				if (sock.Recive(&cData[0], LENGTH_OF_COMMAND, ClientAdress, MSG_PEEK))
 				{
-					if (connections.find(ClientAdress) == connections.end())
-						connections[ClientAdress] = Connection(ClientAdress, &sock);
-					connections[ClientAdress].WorkOn(&cData[0], LENGTH_OF_COMMAND);
-					if (connections[ClientAdress].IsReciveSuccesfull())
+					char comm; int size, id;
+					Connection().GetFromHeader(&cData[0], comm, size, id);
+					if (comm == 'H')
 					{
-						connections[ClientAdress].RelaxRecive();
+						if (connections.find(id) == connections.end())
+						{
+							id = RecivedNewConnection(ClientAdress, id);
+					    }
+						else
+						{
+							connections[size] = connections[id];
+							connections.erase(id);
+							id = size;
+						}
 					}
-					time(&now);
+					if (connections.find(id) != connections.end())
+					{
+						connections[id].WorkOn(&cData[0]);
+						if (connections[id].IsReciveSuccesfull())
+						{
+							connections[id].RelaxRecive();
+						}
+					}
+					else { sock.Recive(&cData[0], LENGTH_OF_COMMAND, ClientAdress); }
 				}
+				time(&now);
 			}
 			if (connections[dest].Status() != WAIT_FOR_COMMAND)
 			{
@@ -434,27 +557,36 @@ namespace logger
 			return true;
 		}
 
-		void CloseConnection(const IPAdress & dest)
+		void CloseConnection(const int & dest)
 		{
 			connections.erase(dest);
+		}
+
+		string InfoString(int id) 
+		{
+			if (connections.find(id) == connections.end()) 
+			{
+				return "error";
+			}
+			return connections[id].IP();
 		}
 
 		void CheckConnection()
 		{
 			time(&now);
-			if (difftime(now, last) > LIVE_TIME)
+			if (difftime(now, last) > LIVE_TIME/3)
 			{
-				for (map <IPAdress, Connection> ::iterator it = connections.begin(); it != connections.end(); ++it)
+				for (map <int, Connection> ::iterator it = connections.begin(); it != connections.end(); ++it)
 				{
 					if (it->second.NoChances(now))
 					{
 						forDelete[it->first] = true;
 					}
 				}
-				for (map <IPAdress, bool> ::iterator it = forDelete.begin(); it != forDelete.end(); ++it)
+				for (map <int, bool> ::iterator it = forDelete.begin(); it != forDelete.end(); ++it)
 				{
-					printf("Connection with %d lost\n", it->first.GetIP());
-					connections.erase(it->first);
+					printf("Connection with %s lost\n", data(connections[it->first].IP()));
+					connections.erase(it->first); 
 				}
 				forDelete.clear();
 				last = now;
